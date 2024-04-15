1310aa3486a14265064fa45fe8e86f0c9b0c6389
JaiswalDeepa <165299012+JaiswalDeepa@users.noreply.github.com>
2024-04-15 18:40:08 +0530

    Add files via upload

---
+++ assign_condition_problem1.js
@@ -0,0 +1,9 @@
+//Write a JavaScript program that compares two variables and prints the larger one
+let a=35
+let b=30
+if(a>b){
+    console.log( a+"is larger than" +b)
+}
+else{
+    console.log(b+ "larger than "+ a)
+}
